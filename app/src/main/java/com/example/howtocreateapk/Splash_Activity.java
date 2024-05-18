71b0379d976dc23ef2befdd851e34dd7b01dfee6
AkshayPrajaCodes <akshaykumarprajapati@hotmail.com>
2024-05-18 19:50:18 +0530

    logo change

--- app/src/main/java/com/example/howtocreateapk/Splash_Activity.java
+++ app/src/main/java/com/example/howtocreateapk/Splash_Activity.java
@@ -2,7 +2,10 @@ package com.example.howtocreateapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class Splash_Activity extends AppCompatActivity {
 
@@ -12,6 +15,18 @@ public class Splash_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        },3000);
+
+        Log.e("TAG", "onCreate: Log Page Change" );
+
+
 
 
     }
