7a7e903b24a9b6a4fb832f1f22804fad902c217d
AkshayPrajaCodes <akshaykumarprajapati@hotmail.com>
2024-05-19 10:35:11 +0530

    log in and log out button backend code complete

--- app/src/main/java/com/example/howtocreateapk/MainActivity.java
+++ app/src/main/java/com/example/howtocreateapk/MainActivity.java
@@ -2,15 +2,36 @@ package com.example.howtocreateapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+
+import com.skydoves.elasticviews.ElasticButton;
 
 public class MainActivity extends AppCompatActivity {
+    ElasticButton btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getSupportActionBar().hide();
+
+        btnLogout=findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences=getSharedPreferences("login",MODE_PRIVATE);
+                SharedPreferences.Editor editor= preferences.edit();
+
+                editor.putBoolean("flag",false);
+
+                Intent intent=new Intent(getApplicationContext(),LogIn_Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
